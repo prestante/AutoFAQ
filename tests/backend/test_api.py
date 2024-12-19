691dc4c226ad20fdb41511f5db219a42d2900a65
Alexander Galkovsky <prestante@yandex.ru>
2024-12-19 16:19:55 +0100

    rewritten api tests with async and httpx

--- tests/backend/test_api.py
+++ tests/backend/test_api.py
@@ -1,5 +1,5 @@
+import httpx
 import pytest
-import requests
 import uuid
 
 BASE_URL = "https://chat.autofaq.ai/api/webhooks/widget/6c24eb52-b1ab-4d78-8463-8556d4ee04b3/messages"
@@ -15,16 +15,20 @@ async def test_post_and_verify_message():
     }
 
     # Отправка POST-запроса
-    post_response = requests.post(BASE_URL, headers=headers, files=message_data)
+    async with httpx.AsyncClient() as client:
+        post_response = await client.post(BASE_URL, headers=headers, files=message_data)
     assert post_response.status_code == 200
 
     # Верификация через GET-запрос
-    get_response = requests.get(BASE_URL, headers=headers)
+    async with httpx.AsyncClient() as client:
+        get_response = await client.get(BASE_URL, headers=headers)
+
     assert get_response.status_code == 200
     messages = get_response.json()
     assert any(msg["text"] == "Test message" for msg in messages)
 
-def test_just_post_message():
+@pytest.mark.asyncio
+async def test_just_post_message():
     session_id = str(uuid.uuid4())
     headers = {
         "Cookie": f"session-id={session_id}"
@@ -33,7 +37,8 @@ def test_just_post_message():
         "message": '{"id": "Some id", "ts": 0, "text": "Some text"}'
     }
 
-    response = requests.post(BASE_URL, headers=headers, files=message_data)
+    async with httpx.AsyncClient() as client:
+        response = await client.post(BASE_URL, headers=headers, files=message_data)
     
     assert response.status_code == 200
     response_body = response.json()
@@ -41,24 +46,28 @@ def test_just_post_message():
     assert response_body["text"] == "Some text"
     assert response_body["sessionId"] == session_id
 
-def test_just_get_messages():
+@pytest.mark.asyncio
+async def test_get_messages():
     session_id = str(uuid.uuid4())
     headers = {
         "Cookie": f"session-id={session_id}"
     }
 
-    response = requests.get(BASE_URL, headers=headers)
+    async with httpx.AsyncClient() as client:
+        response = await client.get(BASE_URL, headers=headers)
     
     assert response.status_code == 200
     response_json = response.json()
     assert isinstance(response_json, list)
 
-def test_post_message_without_body():
+@pytest.mark.asyncio
+async def test_post_message_without_body():
     session_id = str(uuid.uuid4())
     headers = {
         "Cookie": f"session-id={session_id}"
     }
 
-    response = requests.post(BASE_URL, headers=headers)
+    async with httpx.AsyncClient() as client:
+        response = await client.post(BASE_URL, headers=headers)
     
     assert response.status_code == 200, 'Намеренная ошибка'
