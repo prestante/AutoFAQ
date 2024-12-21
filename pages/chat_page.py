1a8ed5105ee0f042d024e5a69306ffbb6c387d79
Alexander Galkovsky <prestante@yandex.ru>
2024-12-21 16:34:53 +0100

    Made frontend tests for opening chat and sending msg

--- pages/chat_page.py
+++ pages/chat_page.py
@@ -10,9 +10,13 @@ class ChatPage(BasePage):
     def open_chat(self):
         self.page.click(chat_button_selector)
 
+    def is_chat_open(self):
+        return self.page.is_visible(message_input_selector)
+
     def send_message(self, message):
-        self.page.fill(message_input_selector, message)
-        self.page.click(send_button_selector)
+        self.page.fill(message_input_selector, message)  # Вводим текст в поле ввода
+        self.page.wait_for_timeout(500)  # Даем странице время на обработку введенного текста
+        self.page.click(send_button_selector)  # Отправляем сообщение
 
     def get_last_message(self):
         return self.page.inner_text(chat_message_selector)
--- tests/frontend/test_chat.py
+++ tests/frontend/test_chat.py
@@ -2,14 +2,19 @@ import pytest
 from pages.chat_page import ChatPage
 from time import sleep
 
-@pytest.mark.frontend
-def test_open_chat(my_page):
+# Фикстура для перехода на страницу и открытия окна чата
+@pytest.fixture(scope="function")
+def chat_page(my_page):
     chat_page = ChatPage(my_page)
-    # print('WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW')
     chat_page.goto("https://autofaq.ai")
-
-    # Click to open the chat widget
     chat_page.open_chat()
+    return chat_page
 
-    # Assert that the chat is open
-    # assert await page.is_visible("#chat21-main-message-context")
+@pytest.mark.frontend
+def test_open_chat(chat_page):
+    assert chat_page.is_chat_open(), "Чат не открыт"
+
+@pytest.mark.frontend
+def test_send_message(chat_page):
+    chat_page.send_message("Привет, бот!")
+    sleep(5)
