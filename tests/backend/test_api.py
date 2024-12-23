22756fbdff99461ed83b251d11d54eb3321137ee
Alexander Galkovsky <prestante@yandex.ru>
2024-12-23 11:12:26 +0100

    Коррекция структуры, тестов и чек листа.

--- tests/backend/test_api.py
+++ tests/backend/test_api.py
@@ -6,30 +6,6 @@ BASE_URL = "https://chat.autofaq.ai/api/webhooks/widget/6c24eb52-b1ab-4d78-8463-
 
 @pytest.mark.backend
 @pytest.mark.asyncio
-async def test_post_and_verify_message():
-    session_id = str(uuid.uuid4())
-    headers = {
-        "Cookie": f"session-id={session_id}"
-    }
-    message_data = {
-        "message": '{"id": "Some id", "ts": 0, "text": "Test message"}'
-    }
-
-    # Отправка POST-запроса
-    async with httpx.AsyncClient() as client:
-        post_response = await client.post(BASE_URL, headers=headers, files=message_data)
-    assert post_response.status_code == 200
-
-    # Верификация через GET-запрос
-    async with httpx.AsyncClient() as client:
-        get_response = await client.get(BASE_URL, headers=headers)
-
-    assert get_response.status_code == 200
-    messages = get_response.json()
-    assert any(msg["text"] == "Test message" for msg in messages)
-
-@pytest.mark.backend
-@pytest.mark.asyncio
 async def test_just_post_message():
     session_id = str(uuid.uuid4())
     headers = {
@@ -73,5 +49,28 @@ async def test_post_message_without_body():
 
     async with httpx.AsyncClient() as client:
         response = await client.post(BASE_URL, headers=headers)
-    
     assert response.status_code == 200, 'Намеренная ошибка'
+    
+@pytest.mark.backend
+@pytest.mark.asyncio
+async def test_post_and_verify_message():
+    session_id = str(uuid.uuid4())
+    headers = {
+        "Cookie": f"session-id={session_id}"
+    }
+    message_data = {
+        "message": '{"id": "Some id", "ts": 0, "text": "Test message"}'
+    }
+
+    # Отправка POST-запроса
+    async with httpx.AsyncClient() as client:
+        post_response = await client.post(BASE_URL, headers=headers, files=message_data)
+    assert post_response.status_code == 200
+
+    # Верификация через GET-запрос
+    async with httpx.AsyncClient() as client:
+        get_response = await client.get(BASE_URL, headers=headers)
+
+    assert get_response.status_code == 200
+    messages = get_response.json()
+    assert any(msg["text"] == "Test message" for msg in messages)
